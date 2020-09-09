3cfde8f113d5a5b9075fe1400b5ffb2470fbebde
simo-grande <smerhay@miu.edu>
2020-09-09 14:32:33 -0500

    Update reverseTests.js

--- reverseTests.js
+++ reverseTests.js
@@ -3,11 +3,11 @@
 describe("reverse", function () {
 
     it("reverse of 4 5 6 is 6 5 4", function () {
-        assert.deepEqual([6, 5, 4], reverseArray([4, 5, 6]));
+        assert.deepEqual(([6, 5, 4]), reverseArray([4, 5, 6]));
     });
 
     it("reverse of 1 2 3 4 is 4 3 2 1", function () {
-        assert.deepEqual([4, 3, 2, 1], reverseArray([1, 2, 3, 4]));
+        assert.deepEqual(([4, 3, 2, 1]), reverseArray([1, 2, 3, 4]));
     });
 
 
