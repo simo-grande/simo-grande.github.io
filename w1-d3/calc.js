d36ff8ad285e71ec2e48f85e36558bd98bbd912a
simo-grande <smerhay@miu.edu>
2020-09-09 22:33:33 -0500

    CALC

---
+++ w1-d3/calc.js
@@ -0,0 +1,19 @@
+/*eslint-disable */
+let calculator = {
+    sum() {
+      return this.a + this.b;
+    },
+  
+    mul() {
+      return this.a * this.b;
+    },
+  
+    read() {
+      this.a = +prompt('a?', 0);
+      this.b = +prompt('b?', 0);
+    }
+  };
+  
+  calculator.read();
+  alert( calculator.sum() );
+  alert( calculator.mul() );
