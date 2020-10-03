382e6878fa4dcff92af691ff130697d8a6a36b9f
simo-grande <smerhay@miu.edu>
2020-10-03 16:21:59 -0500

    w2-d5

---
+++ w2-d5/1.js
@@ -0,0 +1,14 @@
+/*eslint-disable */
+function myMap(arr, fun) {
+    let temp = [];
+    for (let key of arr) {
+        temp.push(fun(key))
+    } return temp;
+}
+function mapper(n) {
+    return n * n;
+}
+
+let arrin = [1, 2, 3, 4, 5];
+console.log("Mapped Array of power 2: ",myMap(arrin, mapper));
+console.log("Orginal Array: ",arrin)
