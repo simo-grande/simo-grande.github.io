68c40ae0820d8365311a97320ddd0792a64a8961
simo-grande <smerhay@miu.edu>
2020-10-08 23:52:12 -0500

    binding

---
+++ w3-d1/partial.js
@@ -0,0 +1,16 @@
+/*eslint-disable*/
+function askPassword(ok, fail) {
+    let password = prompt("Password?", '');
+    if (password == "rockstar") ok();
+    else fail();
+}
+
+let user = {
+    name: 'John',
+
+    login(result) {
+        console.log(this.name + (result ? ' logged in' : ' failed to log in'));
+    }
+};
+
+askPassword(user.login.bind(user,true),user.login.bind(user,false));
