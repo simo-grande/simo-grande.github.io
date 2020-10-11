7c8e149ff28a60cd40dedf4ff20280f9ef890609
simo-grande <smerhay@miu.edu>
2020-10-10 20:27:59 -0500

    hummers

---
+++ w3-d5/4.js
@@ -0,0 +1,23 @@
+/* eslint-disable*/
+let hamster = {
+    stomach: [],
+  
+    eat(food) {
+      this.stomach.push(food);
+    }
+  };
+  
+  let speedy = {
+    __proto__: hamster
+  };
+  
+  let lazy = {
+    __proto__: hamster
+  };
+  
+  // This one found the food
+  speedy.eat("apple");
+  console.log( speedy.stomach ); // apple
+  
+  // This one also has it, why? fix please.
+  console.log( lazy.stomach ); // apple
