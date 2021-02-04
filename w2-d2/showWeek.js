4d923a8dba7d09bd8393d74171f34f2ad97a1d8b
simo-grande <smerhay@miu.edu>
2021-02-04 06:49:01 -0600

    project-CS-418

--- w2-d2/showWeek.js
+++
@@ -1,9 +0,0 @@
-/*eslint-disable */
-function getWeekDay(date) {
-    let days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
-
-    return days[date.getDay()];
-}
-
-let date = new Date(2020, 8, 29);
-console.log(getWeekDay(date));
